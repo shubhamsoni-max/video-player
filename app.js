7b94f8973ce2a23f09c870f207205ae996057977
shubhamsoni-max <chintusoni250796@gmail.com>
2021-02-08 10:35:16 +0530

    add input devices

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
+
 var options = {
   controls: true,
   bigPlayButton: false,
   width: 320,
   height: 240,
+
   plugins: {
     record: {
       audio: true,
@@ -12,11 +15,15 @@ var options = {
       debug: true,
     },
   },
+  
 };
+
+// apply some workarounds for certain browsers
+// applyAudioWorkaround();
 
 var player = videojs("myVideo", options, function () {
   // print version information at startup
-  let vjs = document.querySelector(".vjs-control-bar");
+  
 
   let myVideo = document.getElementById("myVideo");
 
@@ -28,6 +35,8 @@ var player = videojs("myVideo", options, function () {
     " and recordrtc " +
     RecordRTC.version;
   videojs.log(msg);
+
+  let vjs = document.querySelector(".vjs-control-bar");
   vjs.insertAdjacentHTML(
     "beforeend",
     `<span class = "video-btn"><i class="fas fa-video"></i></span>`
@@ -36,37 +45,62 @@ var player = videojs("myVideo", options, function () {
     "beforeend",
     `<span class = "mice-btn"><i class="fas fa-microphone-alt"></i></span>`
   );
-
-  // select classes
+  
 });
 
-// error handling
-player.on("deviceError", function () {
-  console.log("device error:", player.deviceErrorCode);
-});
 
-player.on("error", function (element, error) {
-  console.error(error);
-});
+document.querySelector(".vjs-device-button").onclick = ()=>{
+  player.record().enumerateDevices();
+ 
+// Change AudioInput
 
-// user clicked the record button and started recording
-player.on("startRecord", function () {
-  // event.preventDefault();
-  // console.log(event);
-  let video_device = document.querySelector(".video-device");
-  let audio_device = document.querySelector(".audio-device");
+  function changeAudioInput(event) {
+    var label = event.target.options[event.target.selectedIndex].text;
+    deviceId = event.target.value;
+  
+    try {
+      // change audio input device
+      player.record().setAudioInput(deviceId);
+      console.log(
+        "Changed audio input to '" +
+          label +
+          "' (deviceId: " +
+          deviceId +
+          ")"
+      );
+    } catch (error) {
+      console.warn(error);
+      // jump back to first output device in the list as it's the default
+      event.target.selectedIndex = 0;
+    }
+  }
+
+// Set videoInput
+
+function changeVideoInput(event) {
+  var label = event.target.options[event.target.selectedIndex].text;
+  deviceId = event.target.value;
+
+  try {
+      // change video input device
+      player.record().setVideoInput(deviceId);
+
+      console.log("Changed video input to '" + label + "' (deviceId: " +
+          deviceId + ")");
+  } catch (error) {
+      console.warn(error);
+
+      // jump back to first output device in the list as it's the default
+      event.target.selectedIndex = 0;
+  }
+}
+
+player.on("enumerateReady", function () {
   let custom_video_select = document.querySelector(".custom-video-select");
   let custom_audio_select = document.querySelector(".custom-audio-select");
   let video_btn = document.querySelector(".video-btn");
   let mice_btn = document.querySelector(".mice-btn");
-  // let option = document.querySelector("option");
-  let currentStream;
-  let enumeratorPromise = navigator.mediaDevices.enumerateDevices();
-  function stopMediaTracks(stream) {
-    stream.getTracks().forEach((track) => {
-      track.stop();
-    });
-  }
+
 
   // list of video devices
 
@@ -75,73 +109,43 @@ player.on("startRecord", function () {
       console.log("enumerateDevices() not supported.");
       return;
     }
-    // navigator.mediaDevices
-    //   .enumerateDevices()
-    //   .then(gotDevices)
-    //   .catch(errorCallback);
-
-    // function gotDevices(devices) {
-    //   custom_video_select.innerHTML = "";
-    //   devices.forEach(function (device) {
-    //     if (device.kind === "videoinput") {
-    //       let option = video_device.appendChild(
-    //         document.createElement("option")
-    //       );
-    //       custom_video_select.length + 1;
-    //       option.text = device.label;
-    //       custom_video_select.append(option);
-    //     }
-    //     console.log(
-    //       device.kind + ": " + device.label + " id = " + device.deviceId
-    //     );
-    //   });
-    // }
+    navigator.mediaDevices
+    .enumerateDevices()
+    .then(gotDevices)
+    .catch((err) => {
+      console.log(err);
+    });
     function gotDevices(mediaDevices) {
       custom_video_select.innerHTML = "";
-      custom_video_select.appendChild(document.createElement("option"));
       let count = 1;
       mediaDevices.forEach((mediaDevice) => {
-        // console.log(mediaDevice);
         if (mediaDevice.kind === "videoinput") {
           const option = document.createElement("option");
           option.value = mediaDevice.deviceId;
-          const label = mediaDevice.label || `Camera ${count++}`;
+          const label = mediaDevice.label || ` ${count++}`;
           const textNode = document.createTextNode(label);
           option.appendChild(textNode);
           custom_video_select.appendChild(option);
         }
       });
-    }
-    if (typeof currentStream !== "undefined") {
-      stopMediaTracks(currentStream);
-    }
-    const videoConstraints = {};
-    if (custom_video_select.value === "") {
-      videoConstraints.facingMode = "environment";
+
+      if (custom_video_select.length == 0) {
+        // no output devices found, disable select
+        option = document.createElement('option');
+        option.text = 'No video input devices found';
+        option.value = undefined;
+        custom_video_select.appendChild(option);
+        custom_video_select.disabled = true;
+        console.warn(option.text);
     } else {
-      videoConstraints.deviceId = { exact: custom_video_select.value };
+        console.info('Total video input devices found:', custom_video_select.length);
     }
-    const constraints = {
-      video: videoConstraints,
-      audio: false,
-    };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        currentStream = stream;
-        myVideo.srcObject = stream;
-        return navigator.mediaDevices.enumerateDevices();
-        console.log("working");
-      })
-      .then(gotDevices)
-      .catch((error) => {
-        console.error(error);
-      });
-    navigator.mediaDevices.enumerateDevices().then(gotDevices);
-    // navigator.mediaDevices.enumerateDevices().then(gotDevices);
+
+    }
+    custom_video_select.addEventListener("change", changeVideoInput);
   });
 
-  // list of audio devices
+ // list of audio devices
   mice_btn.addEventListener("click", () => {
     if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
       console.log("enumerateDevices() not supported.");
@@ -154,45 +158,55 @@ player.on("startRecord", function () {
         console.log(err);
       });
 
-    function gotDevices(devices) {
-      custom_audio_select.innerHTML = "";
-      devices.forEach(function (device) {
-        if (device.kind === "audioinput") {
-          let option = audio_device.appendChild(
-            document.createElement("option")
-          );
-          custom_audio_select.length + 1;
-          option.text = device.label;
-          custom_audio_select.append(option);
-        }
-      });
-    }
+      function gotDevices(mediaDevices) {
+        custom_audio_select.innerHTML = "";
+        let count = 1;
+        mediaDevices.forEach((mediaDevice) => {
+          if (mediaDevice.kind === "audioinput") {
+            const option = document.createElement("option");
+            option.value = mediaDevice.deviceId;
+            const label = mediaDevice.label || ` ${count++}`;
+            const textNode = document.createTextNode(label);
+            option.appendChild(textNode);
+            custom_audio_select.appendChild(option);
+          }
+        });
 
-    if (typeof currentStream !== "undefined") {
-      stopMediaTracks(currentStream);
-    }
-    const audioConstraints = {};
-    if (custom_audio_select.value === "") {
-      audioConstraints.facingMode = "environment";
-    } else {
-      audioConstraints.deviceId = { exact: custom_audio_select.value };
-    }
-    const constraints = {
-      video: false,
-      audio: audioConstraints,
-    };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        currentStream = stream;
-        myVideo.srcObject = stream;
-        return navigator.mediaDevices.enumerateDevices();
-      })
-      .then(gotDevices)
-      .catch((error) => {
-        console.error(error);
-      });
+        if (custom_audio_select.length == 0) {
+          // no output devices found, disable select
+          option = document.createElement('option');
+          option.text = 'No audio input devices found';
+          option.value = undefined;
+          custom_audio_select.appendChild(option);
+          custom_audio_select.disabled = true;
+          console.warn(option.text);
+      } else {
+          console.info('Total audio input devices found:', custom_audio_select.length);
+      }
+      }
+      custom_audio_select.addEventListener("change", changeAudioInput);
   });
+});
+
+}
+
+// error handling
+
+player.on("enumerateError", function () {
+  console.log("enumerate error:", player.enumerateErrorCode);
+});
+
+player.on("deviceError", function () {
+  console.log("device error:", player.deviceErrorCode);
+});
+
+player.on("error", function (element, error) {
+  console.error(error);
+});
+
+// user clicked the record button and started recording
+player.on("startRecord", function () {
+  
 
   console.log("started recording!");
 });
